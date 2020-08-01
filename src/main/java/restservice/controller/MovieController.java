3690952172fb7aa73495632c3841f002ce9ecdee
bastalot <bastalot@gmail.com>
2020-08-01 15:56:08 +0200

    mozliwosc nadpisania po id mozliwosc usuniecia po id

--- src/main/java/restservice/controller/MovieController.java
+++ src/main/java/restservice/controller/MovieController.java
@@ -1,12 +1,17 @@
 package restservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import restservice.entity.Movie;
 import restservice.repository.MovieRepository;
 
+import javax.validation.Valid;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class MovieController {
@@ -27,6 +32,38 @@ public class MovieController {
     @GetMapping("/movie/{id_movie}")
     Movie one(@PathVariable Integer id_movie) {
         return repository.findOne(id_movie);
+    }
+
+    @PutMapping("/movie/{id_movie}")
+    ResponseEntity<Movie> update(@PathVariable Integer id_movie, @RequestBody Movie movieDetails) throws ResourceNotFoundException {
+        Movie movie = repository.findOne(id_movie);
+
+        if (movieDetails.getTitle() != null)
+        movie.setTitle(movieDetails.getTitle());
+        if (movieDetails.getRelease_date() != null)
+        movie.setRelease_date(movieDetails.getRelease_date());
+        if (movieDetails.getRuntime() != null)
+        movie.setRuntime(movieDetails.getRuntime());
+        if (movieDetails.getSummary() != null)
+        movie.setSummary(movieDetails.getSummary());
+        if (movieDetails.getPoster() != null){
+            movie.setPoster(movieDetails.getPoster());
+        }
+
+
+        final Movie updatedmovie = repository.save(movieDetails);
+        return ResponseEntity.ok(updatedmovie);
+
+    }
+
+    @DeleteMapping("/movie/{id_movie}")
+    Map<String, Boolean> deleteMovie(@PathVariable Integer id_movie) throws ResourceNotFoundException {
+       Movie movie = repository.findOne(id_movie);
+       repository.delete(movie);
+       Map<String, Boolean> response = new HashMap<>();
+       response.put("deleted", Boolean.TRUE);
+       return response;
+
     }
 
    /* @GetMapping("/movie/{id_movie}/poster")
