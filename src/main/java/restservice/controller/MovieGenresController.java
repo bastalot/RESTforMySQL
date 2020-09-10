f9614257af3017c73fa3e4c44a7420fb3a697ed1
bastalot <bastalot@gmail.com>
2020-09-10 22:22:52 +0200

    wiecej rzeczy

--- src/main/java/restservice/controller/MovieGenresController.java
+++ src/main/java/restservice/controller/MovieGenresController.java
@@ -1,6 +1,6 @@
 package restservice.controller;
 
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
